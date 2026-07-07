c66bd1da69f795a984076b014eead1099254ab87
Vijay <vijayvijay36362@gmail.com>
2026-07-07 10:54:45 +0530

    redirect pagrs done

--- hello_flask.py
+++ hello_flask.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/login")
-def template():
-    return render_template ("index.html")
-    
+@app.route("/pageone")
+def firstpage():
+    return render_template("firstpage.html")
+
+@app.route("/pagetwo")
+def secondpage():
+    return render_template("secondpage.html")
+
 if __name__ == "__main__":
-    app.run()   
+    app.run()
