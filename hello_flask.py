0b2340356ee81173c393dbdccd91067f4097fee3
Vijay <vijayvijay36362@gmail.com>
2026-07-07 11:31:40 +0530

    inner redirect done

--- hello_flask.py
+++ hello_flask.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+import datetime
 
 app = Flask(__name__)
 
-@app.route("/pageone")
-def firstpage():
-    return render_template("firstpage.html")
+@app.route("/frontpage")
+def frontpage():
+    return render_template("frontpage.html")
 
-@app.route("/pagetwo")
-def secondpage():
-    return render_template("secondpage.html")
+@app.route("/resultpage")
+def resultpage():
+    return render_template("resultpage.html")
+
+@app.route("/printtime")
+def printtime():
+    print()
+    print(datetime.datetime.now())
+    print()
+    return redirect("/resultpage")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
